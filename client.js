86e238f1d4dda0ab6bbd1dcee9501dac394d1fa2
Sebastian Kann <sebastiankann@gmail.com>
2020-09-23 17:09:50 +0000

    separated out a client.js file from the play.js file

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: '135.23.222.131',
+    port: 50542
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (message) => {
+    console.log(message);
+  })
+  
+  return conn;
+};
+
+module.exports = {connect};
+
+// console.log('Connecting ...');
+// connect();
--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '135.23.222.131',
-    port: 50542
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on('data', (message) => {
-    console.log(message);
-  })
-  
-  return conn;
-};
+const {connect} = require('./client')
 
 console.log('Connecting ...');
 connect();
