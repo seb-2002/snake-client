5fe1028b825a0b2960fa6385a6fe47db0d331b4c
Sebastian Kann <sebastiankann@gmail.com>
2020-09-23 19:37:33 +0000

    setup a UI for the game

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
 const {connect} = require('./client')
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  const handleUserInput = function() {
+    stdin.on('data', (key) => {
+      if (key === "\u0003") {
+        process.exit();
+      }
+      return key;
+    })
+  }
+  
+  handleUserInput();
+  return stdin
+}
+
+
 console.log('Connecting ...');
 connect();
+setupInput();
