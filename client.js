5fa7dfde10b7d18e44100f518edc4e308f608f88
Sebastian Kann <sebastiankann@gmail.com>
2020-09-23 22:43:19 +0000

    added a constants.js

--- client.js
+++ client.js
@@ -1,32 +1,30 @@
-const net = require('net');
+const net = require("net");
+const { IP, PORT } = require("./constants");
 
-const connect = function() {
-  
-  
+const connect = function () {
   const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
+    host: IP,
+    port: PORT,
   });
 
   // set the encoding to read text
-  conn.setEncoding('utf8');
+  conn.setEncoding("utf8");
 
   // when the client connects...
-  conn.on('connect', () => {
-    console.log('Successfully connected to the server!'); // log a welcome message
-    conn.write('Name: SEK') // write a name to the server
+  conn.on("connect", () => {
+    console.log("Successfully connected to the server!"); // log a welcome message
+    conn.write("Name: SEK"); // write a name to the server
   });
 
-
   // if the client recieves a message from the server ...
-  conn.on('data', (message) => {
+  conn.on("data", (message) => {
     console.log(message); // print it to the console
-  })
-  
+  });
+
   return conn; // the function returns the connection itself
 };
 
-module.exports = {connect};
+module.exports = { connect };
 
 // console.log('Connecting ...');
 // connect();
